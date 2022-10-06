af11f09f2f5dd4519ab5d57ea018cc28ddc9ca33
aalvalencia <aalvalencia@Pleasure-Man.localdomain>
2022-10-06 11:18:59 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+1. HTML/ Javascript
+2. Next.js and react.js
---
+++ index.js
@@ -0,0 +1,79 @@
+import { ethers } from "./ethers-5.6.esm.min.js"
+import { abi, contractAddress } from "./constants.js"
+
+const connectButton = document.getElementById("connectButton")
+const fundButton = document.getElementById("fundButton")
+const withdrawButton = document.getElementById("withdrawButton")
+const balanceButton = document.getElementById("balanceButton")
+connectButton.onclick = connect
+fundButton.onclick = fund
+balanceButton.onclick = getBalance
+withdrawButton.onclick = withdraw
+
+async function connect() {
+    if (typeof window.ethereum !== "undefined") {
+        console.log("Metamask Installed!")
+        await window.ethereum.request({ method: "eth_requestAccounts" }) //connects to metamask :)
+        connectButton.innerHTML = "Connected"
+    } else {
+        connectButton.innerHTML = "Please install metamask"
+    }
+}
+
+async function getBalance() {
+    if (typeof window.ethereum !== "undefined") {
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        const balance = await provider.getBalance(contractAddress)
+        console.log(ethers.utils.formatEther(balance))
+    }
+}
+
+async function fund() {
+    const ethAmount = document.getElementById("ethAmount").value
+    console.log(`Fundiong with ${ethAmount}...`)
+    if (typeof window.ethereum !== "undefined") {
+        //provider / connection to the blockchain
+        //signer / wallet / someone with some gas
+        //contract we are interacting using ABI and Address
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        const signer = provider.getSigner()
+        const contract = new ethers.Contract(contractAddress, abi, signer) //abi
+        try {
+            const transactionResponse = await contract.fund({
+                value: ethers.utils.parseEther(ethAmount),
+            })
+            await listenForTransactionMine(transactionResponse, provider)
+            console.log("Done!")
+        } catch (e) {
+            console.log(e)
+        }
+    }
+}
+
+function listenForTransactionMine(transactionResponse, provider) {
+    console.log(`Mining ${transactionResponse.hash}....`)
+    return new Promise((resolve, reject) => {
+        provider.once(transactionResponse.hash, (transactionReceipt) => {
+            console.log(
+                `Completed with ${transactionReceipt.confirmations} confirmations`
+            )
+            resolve()
+        })
+    })
+    //create listener
+}
+
+async function withdraw() {
+    if (typeof window.ethereum !== "undefined") {
+        console.log("Withdrawing...")
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        const signer = provider.getSigner()
+        const contract = new ethers.Contract(contractAddress, abi, signer)
+        try {
+            const transactionResponse = await contract.withdraw()
+            await listenForTransactionMine(transactionResponse, provider)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
